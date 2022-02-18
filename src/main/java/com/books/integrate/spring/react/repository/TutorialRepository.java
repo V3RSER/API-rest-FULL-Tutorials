e74a288e90006c25f37973140542bac3d8974df5
V3RSER <78446571+V3RSER@users.noreply.github.com>
2022-02-18 14:11:57 -0500

    Agregado endPoint para eliminar por titulo

--- src/main/java/com/books/integrate/spring/react/repository/TutorialRepository.java
+++ src/main/java/com/books/integrate/spring/react/repository/TutorialRepository.java
@@ -1,11 +1,12 @@
 package com.books.integrate.spring.react.repository;
 
-import java.util.List;
-
 import com.books.integrate.spring.react.model.Tutorial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
-	List<Tutorial> findByPublished(boolean published);
-	List<Tutorial> findByTitleContaining(String title);
+    List<Tutorial> findByPublished(boolean published);
+
+    List<Tutorial> findByTitleContaining(String title);
 }
